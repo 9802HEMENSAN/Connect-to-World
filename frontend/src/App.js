555b0aa1df11f1f354bf69e2f6d8c043ea6abd80
9802HEMENSAN <hmahilange9802@gmail.com>
2023-08-23 08:47:51 +0530

    frontend for authenticaiton created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+import { useEffect } from "react";
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { loginWithRedirect, logout, user, isAuthenticated, isLoading } =
+    useAuth0();
+
+  const LoginUser = async ()=>{
+    try {
+      const userDetail = {
+        email : user.email,
+        name : user.name,
+        picture : user.picture,
+        sub : user.sub
+      }
+      const userResponse = await fetch("http://localhost:8080/user/register", { 
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body : JSON.stringify(userDetail)
+      })
+      const userDetails = await userResponse.json()
+      console.log(userDetails)
+      setTimeout(() => {
+        alert(userDetails.msg)
+      }, 3000);
+    } catch (error) {
+      console.log("error: " + error)
+      alert("Login Failed !!")
+    }
+  }
+
+  useEffect(() => {
+     if (isAuthenticated) {
+       console.log(user);
+       console.log(isAuthenticated);
+        LoginUser()
+     }
+  },[isAuthenticated])
+ 
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+      <button
+        onClick={() =>
+          logout({ logoutParams: { returnTo: window.location.origin } })
+        }
+      >
+        Log Out
+      </button>
+      {isAuthenticated && (
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+      domain="dev-65slr4mj7tngkw2k.us.auth0.com"
+      clientId="pNmJQarbFloQX9dEYPfARDfrWHIA0gov"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
